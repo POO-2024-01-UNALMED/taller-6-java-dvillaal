07e904247be56ef2c429d14e78280bb259abd6d5
dvillaal <dvillaal@unal.edu.co>
2024-04-02 09:00:20 -0500

    2

--- src/main/java/vehiculos/Automóvil.java
+++ src/main/java/vehiculos/Automóvil.java
@@ -2,5 +2,16 @@ package vehiculos;
 
 public class Automóvil extends Vehículo {
     private int puestos;
+    public Automóvil(String placa, int puertas, String nombre, int precio, int peso, Fabricante fabricante, int puestos){
+        super(placa, 4, 100, nombre, precio, peso,"FWD",fabricante );
+        this.puestos=puestos;
+    }
+
+    public int getPuestos() {
+        return puestos;
+    }
+    public void setPuestos(int puestos){
+        this.puestos = puestos;
+    }
 
 }
--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -2,5 +2,9 @@ package vehiculos;
 
 public class Camioneta extends Vehículo {
     private boolean volco;
+    public Camioneta(String placa, int puertas, String nombre, int precio, int peso, String traccion, Fabricante fabricante, boolean volco){
+        super(placa ,puertas ,90, nombre, precio, peso,"4X4",fabricante );
+        this.volco=volco;
+    }
 
 }
