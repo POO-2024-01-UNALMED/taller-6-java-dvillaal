e9416213777fe9177b4902aa872c05304d833ac4
dvillaal <dvillaal@unal.edu.co>
2024-04-02 10:35:01 -0500

    5

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,7 +1,46 @@
 package vehiculos;
 
+import java.util.ArrayList;
+
 public class Fabricante {
     private String nombre;
     private País país;
+    private int vehiculosCreados;
+    private ArrayList<Vehículo> vehículos = new ArrayList<Vehículo>();
+    private static ArrayList<Fabricante> fabricantes;
 
+    public Fabricante(String nombre, País país){
+        this.nombre = nombre;
+        this.país = país;
+
+        if (fabricantes == null){
+            fabricantes = new ArrayList<Fabricante>();
+        }
+
+        fabricantes.add(this);
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public País getPaís(){
+        return país;
+    }
+    public void setPaís(País país){
+        this.país = país;
+    }
+
+    public int getVehiculosCreados(){
+        return vehiculosCreados;
+    }
+
+    public void añadirVehiculo(Vehículo vehículo){
+        this.vehículos.add(vehículo);
+        this.país.añadirVehiculo(vehículo);
+        this.vehiculosCreados++;
+    }
 }
--- src/main/java/vehiculos/País.java
+++ src/main/java/vehiculos/País.java
@@ -1,5 +1,36 @@
 package vehiculos;
 
+import java.util.ArrayList;
+
 public class País {
     private String nombre;
+    private int vehiculosCreados;
+    private ArrayList<Vehículo> vehículos = new ArrayList<Vehículo>();
+    private static ArrayList<País> países;
+
+    public País(String nombre){
+        this.nombre = nombre;
+
+        if (países == null){
+            países = new ArrayList<País>();
+        }
+
+        países.add(this);
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public int getVehiculosCreados(){
+        return vehiculosCreados;
+    }
+
+    public void añadirVehiculo (Vehículo vehículo){
+        this.vehículos.add(vehículo);
+        this.vehiculosCreados++;
+    }
 }
