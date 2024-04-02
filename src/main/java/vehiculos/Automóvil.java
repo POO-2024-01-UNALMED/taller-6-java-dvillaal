4c414f8f22f713a69d85ffacca2cc98d34f60635
dvillaal <dvillaal@unal.edu.co>
2024-04-02 11:28:15 -0500

    Update Automóvil.java

--- src/main/java/vehiculos/Automóvil.java
+++ src/main/java/vehiculos/Automóvil.java
@@ -3,6 +3,7 @@ package vehiculos;
 public class Automóvil extends Vehículo {
     private int puestos;
     private static int CantidadVehiculos;
+    
     public Automóvil(String placa, int puertas, String nombre, int precio, int peso, Fabricante fabricante, int puestos){
         super(placa, 4, 100, nombre, precio, peso,"FWD",fabricante );
         this.puestos=puestos;
